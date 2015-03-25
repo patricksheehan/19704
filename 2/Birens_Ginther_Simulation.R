f7b831499e418e69a3b7168592374c64506c0f26
patricksheehan <Patrick@PJS.wv.cc.cmu.edu>
2015-03-25 14:27:43 -0400

    Small modification to simulation file

--- 2/Birens_Ginther_Simulation.R
+++ 2/Birens_Ginther_Simulation.R
@@ -1,5 +1,6 @@
 # Author: Patrick Sheehan
 # Purpose: Replicate Table 1.A and 2.A from Bierens and Ginther (2001)
+#          Extend the replication
 # Input: None
 # Output(s): t-values and estimates from Table 1.A (T.1A) and 2.A (T.2A)
 
@@ -7,7 +8,6 @@
 library(AER) # Data
 library(quantreg) # LAD regression
 data(CPS1988)
-
 # Simulate the LAD median regression of Table 1.A:
 T.1A <- rq(log(wage) ~ education + experience + I(experience^2) + ethnicity,
            tau=0.5, data=CPS1988, model=TRUE, method="fn")
@@ -17,3 +17,5 @@ T.2A <- rq(log(wage) ~ education + experience + I(experience^2) +
                        I(experience^3) + I(experience^4) + ethnicity,
            tau=0.5, data=CPS1988, model=TRUE, method="fn")
 summary(T.2A) # Get detailss of the LAD regression
+
+
