579e9579ec1029b804fadb5016e2f2f2ce5bfff6
patricksheehan <Patrick@PJS.wv.cc.cmu.edu>
2015-03-25 14:24:49 -0400

    Finished histogram extension, made separate file for simplicity

---
+++ 2/Birens_Ginther_Extension_Histograms.R
@@ -0,0 +1,39 @@
+# Author: Patrick Sheehan
+# Purpose: Extend the replication of Table 1.A and 2.A 
+#          from Bierens and Ginther (2001)      
+# Input: None (But requires "Birens_Ginther_Simulation.R" in working directory)
+# Output(s): Histograms
+
+# First, run my simulation replicating Table 1.A and 2.a
+source("Birens_Ginther_Simulation.R")
+# Part 1: Histograms (wages, log(wages), education, experience)
+wages <- CPS1988$wage # Get the data 
+log.wages <- log(wages)
+education <- CPS1988$education
+experience <- CPS1988$experience
+pdf("histograms.pdf") # Begin PDF graphics driver
+wage.hist <- hist(wages,
+     breaks = c(seq(0, 3500, 50),max(wages)),
+     freq = TRUE,
+     main = "Wages",
+     xlim = c(0, 3.5e3),
+     xlab = "Wages (in dollars per week)")
+axis(1, at=c(0, 50, seq(500, 3500, 500)))
+hist(log.wages,
+     breaks = "Scott",
+     main = "log(Wages)",
+     xlab = "log(Wages)")
+ed.hist <- hist(education,
+                breaks = seq(0, max(education)+1, 1), # One bin for each year
+                right = FALSE, # Mandate one year for each bin
+                main = "Education",
+                xlab = "Education (discrete years)",
+                xaxt = "n")
+axis(1, at=ed.hist$mids, labels=seq(0, max(education), 1), cex.axis=0.8,
+     tick=FALSE, line=-2)
+hist(experience,
+     main = "Experience",
+     xlab = "Experience (discrete years)",
+     breaks = seq(min(experience), max(experience)+1, 1), # One bin per year
+     right = FALSE,) # Mandate one year per bin
+dev.off()
